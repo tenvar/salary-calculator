6ea823e4e06f6a048f38d9997497786c65e19b7b
Itay <presman97@gmail.com>
2026-01-30 00:28:36 +0200

    V1

---
+++ src/utils/locales.js
@@ -0,0 +1,176 @@
+export const locales = {
+    en: {
+        title: "Israel Salary Calculator",
+        subtitle: "Estimate your net salary for 2025/2026",
+        sections: {
+            financial: "Financial Data",
+            personal: "Personal Details",
+            results: "Results"
+        },
+        labels: {
+            baseSalary: "Base Salary (Brutto)",
+            bonuses: "Bonuses & Commissions",
+            travel: "Travel (Nesi'ot)",
+            havraa: "Recuperation (Dmei Havra'a)",
+            nonSocial: "Non-social Additions",
+            carValue: "Car Value (Shovi Rechev)",
+            foodValue: "Food/Phone Value",
+            otherBenefits: "Other Benefits",
+
+            gender: "Gender",
+            male: "Male",
+            female: "Female",
+
+            resident: "Residency Status",
+            regular: "Resident",
+            oleh: "New Immigrant (Oleh Hadash)",
+
+            children: "Children",
+            children05: "Ages 0-5",
+            children617: "Ages 6-17",
+            children18: "Age 18",
+            childrenFather03: "Ages 0-3 (Father)",
+
+            aliyahMonths: "Months since Aliyah",
+
+            army: "Military Service",
+            none: "None / Not Served",
+            less24: "Less than 24 months (or Woman)",
+            more24: "More than 24 months (Man)",
+
+            degree: "Academic Degree",
+            noDegree: "None",
+            bachelor: "Bachelor's Degree",
+            master: "Master's Degree"
+        },
+        results: {
+            taxableIncome: "Taxable Income",
+            grossTax: "Gross Income Tax",
+            points: "Credit Points",
+            pointsValue: "Points Value",
+            finalTax: "Final Income Tax",
+            bituahLeumi: "Bituah Leumi",
+            healthTax: "Health Tax",
+            pension: "Pension",
+            studyFund: "Keren Hishtalmut",
+            totalDeductions: "Total Deductions",
+            netSalary: "Net Salary"
+        }
+    },
+    he: {
+        title: "מחשבון שכר ישראל",
+        subtitle: "חישוב משוער של שכר נטו לשנת 2025/2026",
+        sections: {
+            financial: "נתונים כספיים",
+            personal: "פרטים אישיים",
+            results: "תוצאות"
+        },
+        labels: {
+            baseSalary: "שכר יסוד (ברוטו)",
+            bonuses: "בונוסים ועמלות",
+            travel: "נסיעות",
+            havraa: "דמי הבראה",
+            nonSocial: "תוספות לא סוציאליות",
+            carValue: "שווי רכב",
+            foodValue: "שווי ארוחות/טלפון",
+            otherBenefits: "הטבות אחרות",
+
+            gender: "מין",
+            male: "זכר",
+            female: "נקבה",
+
+            resident: "תושבות",
+            regular: "תושב",
+            oleh: "עולה חדש",
+
+            children: "ילדים",
+            children05: "גילאי 0-5",
+            children617: "גילאי 6-17",
+            children18: "גיל 18",
+            childrenFather03: "גילאי 0-3 (אב)",
+
+            aliyahMonths: "חודשים מאז העלייה",
+
+            army: "שירות צבאי",
+            none: "לא שירת/ה",
+            less24: "פחות מ-24 חודשים (או אישה)",
+            more24: "יותר מ-24 חודשים (גבר)",
+
+            degree: "תואר אקדמי",
+            noDegree: "ללא",
+            bachelor: "תואר ראשון",
+            master: "תואר שני"
+        },
+        results: {
+            taxableIncome: "הכנסה חייבת",
+            grossTax: "מס הכנסה (גולמי)",
+            points: "נקודות זיכוי",
+            pointsValue: "שווי נקודות",
+            finalTax: "מס הכנסה לתשלום",
+            bituahLeumi: "ביטוח לאומי",
+            healthTax: "מס בריאות",
+            pension: "פנסיה",
+            studyFund: "קרן השתלמות",
+            totalDeductions: "סך הכל ניכויים",
+            netSalary: "שכר נטו"
+        }
+    },
+    ru: {
+        title: "Калькулятор Зарплаты Израиль",
+        subtitle: "Расчет чистой зарплаты (нетто) на 2025/2026 год",
+        sections: {
+            financial: "Финансовые данные",
+            personal: "Личные данные",
+            results: "Результаты"
+        },
+        labels: {
+            baseSalary: "Базовая зарплата (Брутто)",
+            bonuses: "Бонусы / Комиссионные",
+            travel: "Проезд (Nesi'ot)",
+            havraa: "Оздоровительные",
+            nonSocial: "Доп. выплаты (без соц.)",
+            carValue: "Стоимость авто (Shovi Rechev)",
+            foodValue: "Питание / Телефон",
+            otherBenefits: "Другие льготы",
+
+            gender: "Пол",
+            male: "Мужской",
+            female: "Женский",
+
+            resident: "Статус резидента",
+            regular: "Резидент",
+            oleh: "Новый репатриант",
+
+            children: "Дети",
+            children05: "0-5 лет",
+            children617: "6-17 лет",
+            children18: "18 лет",
+            childrenFather03: "0-3 года (Отец)",
+
+            aliyahMonths: "Месяцев с репатриации",
+
+            army: "Служба в армии",
+            none: "Не служил",
+            less24: "< 24 мес (или жен)",
+            more24: "> 24 мес (муж)",
+
+            degree: "Образование",
+            noDegree: "Нет",
+            bachelor: "Бакалавр",
+            master: "Магистр"
+        },
+        results: {
+            taxableIncome: "Налогооблагаемая база",
+            grossTax: "Налог (Gross)",
+            points: "Льготные баллы",
+            pointsValue: "Скидка за баллы",
+            finalTax: "Итоговый подоходный налог",
+            bituahLeumi: "Битуах Леуми",
+            healthTax: "Налог на здоровье",
+            pension: "Пенсия",
+            studyFund: "Керен Иштальмут",
+            totalDeductions: "Всего удержаний",
+            netSalary: "Нетто (На руки)"
+        }
+    }
+};
